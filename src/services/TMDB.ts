92a3b5c59ede33450bef1da31d78e762e827dbe8
thekingjosha <kingjoshuaerughe@gmail.com>
2024-07-23 16:27:19 +0100

    Added query to fetch Movies

--- src/services/TMDB.ts
+++ src/services/TMDB.ts
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const tmdbApiKey = import.meta.env.VITE_TMBD_KEY
-const page = 1
+// const page = 1
 
 export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
@@ -9,7 +9,20 @@ export const tmdbApi = createApi({
     endpoints: (builder) => ({
         //* Get Movies by [Type]
         getMovies: builder.query({
-            query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`
+            query: ({genreIdOrCategoryName, page}) =>{
+                console.log(page)
+                // Get Movies by Category
+                if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string'){
+                    return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
+                }
+                // Get Movies by Genre
+                if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number'){
+                    return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`
+
+                }
+                // Get Popular Movies
+                return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+            } 
         }),
         // Get movie by [type]
         getGenres: builder.query({
