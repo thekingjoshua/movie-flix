4145d00c161be97713f071816c5ade4f993aeb5f
KingJoshua <kingjayeriughe@gmail.com>
2024-07-24 23:52:55 +0100

    Implemented Search functionality

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,43 +4,62 @@ import {Search as SearchIcon} from '@mui/icons-material'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 import {useTheme} from '@mui/material/styles'
+import {styled} from '@mui/system'
+import { searchMovie } from '../../features/currentGenreOrCatergory'
 
+const SearchContainer = styled('div')(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+    },
+  }));
 
+  const Input = styled(TextField)(({ theme }) => ({
+    '& .MuiInputBase-input': {
+      color: theme.palette.mode === 'light' ? 'white' : 'black',
+    },
+    '& .MuiInput-underline:before': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'white',
+    },
+    filter: theme.palette.mode === 'light' ? 'light' : 'invert(1)',
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '-10px',
+      marginBottom: '10px',
+    },
+  }));
 
 const Search = () => {
     const theme = useTheme()
     const [query, setQuery] =  useState<string>()
+    const dispatch = useDispatch()
     const classes = {
-        searchContainer: {
-            [theme.breakpoints.down('sm')]: {
-                display: 'flex',
-                justifyContent: 'center',
-                width: '100%'
-            }
-        },
-        input: {
-            color: theme.palette.mode === 'light' ? 'light' : 'black',
-            filter: theme.palette.mode === 'light' ? 'light' : 'invert(1)',
-            [theme.breakpoints.down('sm')]: {
-                marginTop: '-10px',
-                marginBottom: '10px'
-            }
-        }
+        searchContainer: {backgroundColor: 'red', display: 'flex', justifyContent: 'center', [theme.breakpoints.down('sm')]: {width: '100%'}},
+        input: { color: theme.palette.mode === 'light' ? 'white' : 'black', borderColor: 'red', filter: theme.palette.mode === 'light' ? 'light' : 'invert(1)', [theme.breakpoints.down('sm')]: { marginTop: '-10px', marginBottom: '10px'}},
+        searchIcon: { color: theme.palette.mode === 'light' ? 'white' : 'black'}
     }
-    const handleKeyPress = () => {
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === 'Enter'){
+            console.log(query)
+            dispatch(searchMovie(query))
+            console.log('working')
+        }
 
     }
     return (
-    <div style={classes.searchContainer}>
-        <TextField onKeyPress={handleKeyPress} value={query} onChange={(e) => setQuery(e.target.value)} variant="standard" 
-        InputProps={{style: classes.input, startAdornment: (
-            <InputAdornment position='start'>
+        <SearchContainer>
+
+        <Input onKeyPress={handleKeyPress} onChange={(e) => setQuery(e.target.value)} variant="standard" InputProps={{sx: classes.input, startAdornment: (
+            <InputAdornment sx={classes.searchIcon} position='start'>
                 <SearchIcon/>
             </InputAdornment>
         )}} 
         
         />
-    </div>
+        </SearchContainer>
   )
 }
 
