b188e75b36c8763c49e9156f6187fc669cc1e4d8
KingJoshua <kingjayeriughe@gmail.com>
2024-07-26 17:15:13 +0100

    Updated profile page to display logged in user details

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,7 +1,14 @@
+import {userSelector} from '../../features/auth'
+import { useSelector } from 'react-redux'
+
 const Profile = () => {
+  const {user} = useSelector(userSelector);
+
   return (
     <div>
-      Profile
+      Profile Details <br/>
+      Current Username is {user.username} <br/>
+      Current User ID = {user.id}
     </div>
   )
 }
