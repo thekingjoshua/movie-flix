c574d5b2bcbbe1d701edbc00ab001983ad43343d
KingJoshua <kingjayeriughe@gmail.com>
2024-07-26 15:51:40 +0100

    Added Auth slice to handle user Authentication

---
+++ src/features/auth.ts
@@ -0,0 +1,28 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const initialState ={
+    user: {},
+    isAuthenticated: false,
+    sessionId: ''
+}
+
+const authSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload;
+            state.isAuthenticated = true;
+            state.sessionId = localStorage.getItem('sessionId') as string
+
+            localStorage.setItem('accountId', action.payload.id)
+        }
+    }
+})
+
+export const {setUser} = authSlice.actions;
+
+export default authSlice.reducer
+
+export const userSelector = (state: any) => state.user
