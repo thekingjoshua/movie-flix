ad98db686e4248bd0dff7e96923a8b43ac444d83
KingJoshua <kingjayeriughe@gmail.com>
2024-07-26 15:52:46 +0100

    Added userReducer for Authentication

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,16 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { tmdbApi } from '../services/TMDB'; 
 import genreOrCategoryReducer from '../features/currentGenreOrCatergory'
+import userReducer from '../features/auth'
 
 export const store = configureStore({
     reducer: {
         [tmdbApi.reducerPath]: tmdbApi.reducer,
         currentGenreOrCategory: genreOrCategoryReducer,
+        user: userReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
 });
 
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
 
+export type AppDispatch = typeof store.dispatch;
