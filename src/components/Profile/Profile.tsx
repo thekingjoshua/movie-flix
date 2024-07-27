28b43b76255838e62763000b6c7d9bfec6db6da0
KingJoshua <kingjayeriughe@gmail.com>
2024-07-27 15:34:04 +0100

    Added Log out functionality to Profile Page

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,15 +1,29 @@
 import {userSelector} from '../../features/auth'
+import {Typography, Button, Box} from '@mui/material'
 import { useSelector } from 'react-redux'
+import { ExitToApp } from '@mui/icons-material'
 
 const Profile = () => {
   const {user} = useSelector(userSelector);
 
+  const logOut = () => {
+    localStorage.clear()
+
+    window.location.href = "/"
+  }
+
+  const favouriteMovies = []
+
   return (
-    <div>
-      Profile Details <br/>
-      Current Username is {user.username} <br/>
-      Current User ID = {user.id}
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logOut}>
+          Logout &nbsp; <ExitToApp/>
+        </Button>
+      </Box>
+      {!favouriteMovies.length ? <Typography variant="h5">Add favourites movies</Typography> : <Box>Favourite Movies</Box> }
+    </Box>
   )
 }
 
