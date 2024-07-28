84d252e0c6fe9911b7cfaeb0ce560f6c46a148f9
KingJoshua <kingjayeriughe@gmail.com>
2024-07-28 19:24:10 +0100

    Fixed type error in Link style props

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,4 +1,4 @@
-import { Typography, Grid, Grow, Tooltip, Rating, useTheme } from "@mui/material"
+import { Typography, Grid, Grow, Tooltip, Rating, useTheme, styled } from "@mui/material"
 import {Link} from 'react-router-dom'
 
 type MovieProps = {
@@ -11,9 +11,25 @@ type MovieProps = {
     i: number
 }
 
+const CLink = styled(Link)(({theme}) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontWeight: 'bolder',
+  textDecoration: 'none',
+  flexDirection: 'column',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  '& :hover': {
+    cursor: 'pointer',
+    transform: 'scale(1.05)'
+  }
+}))
+
 const Movie = ({movie, i}: MovieProps) => {
     const theme = useTheme() 
-
     const classes = {
         movie: {
             padding: '10px',
@@ -28,29 +44,11 @@ const Movie = ({movie, i}: MovieProps) => {
             marginBottom: 0,
             textAlign: 'center'
         },
-        links: {
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          fontWeight: 'bolder',
-          textDecoration: 'none',
-          flexDirection: 'column',
-          [theme.breakpoints.up('sm')]: {
-            display: 'flex',
-            flexDirection: 'column',
-            
-          },
-          '&:hover': {
-            cursor: 'pointer',
-            color: 'red !important',
-            display: 'none'
-          }
-        },
         image: {
           borderRadius: '20px',
           height: '300px',
           marginBottom: '10px',
-          '&:hover': {
+          '&: hover': {
             transform: 'scale(1.05)'
           }
         }
@@ -59,15 +57,15 @@ const Movie = ({movie, i}: MovieProps) => {
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} sx={classes.movie}>
       <Grow in key={i} timeout={(i + 1) * 300}>
-        <Link style={classes.links} to={`/movie/${movie.id}`}>
-          <img alt={movie.title.toString()} style={classes.image} src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "https://www.fillmurray.com/200/300"} />
+        <CLink to={`/movie/${movie.id}`}>
+        <img alt={movie.title.toString()} style={classes.image} src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "https://www.fillmurray.com/200/300"} />
             <Typography sx={classes.title} variant="h5">{movie.title}</Typography>
             <Tooltip disableFocusListener title={`${movie.vote_average.toFixed(1)} / 10`}>
                 <div>
                   <Rating readOnly value={movie.vote_average  / 2} precision={0.1} />
                 </div>
             </Tooltip>
-        </Link>
+        </CLink>
       </Grow>
     </Grid>
   )
