f59e9ed7399e9e76e308daaf24bbdd5476c9fcfe
KingJoshua <kingjayeriughe@gmail.com>
2024-07-28 19:50:02 +0100

    Added styling to Movie Information Page

--- src/components/MovieInformation/MovieInformation.tsx
+++ src/components/MovieInformation/MovieInformation.tsx
@@ -1,8 +1,22 @@
-import {Grid, Box, CircularProgress} from '@mui/material'
+import {Grid, Box, CircularProgress, styled} from '@mui/material'
 import {Link, useParams} from 'react-router-dom'
 import { useGetMovieQuery } from '../../services/TMDB'
 import {useTheme} from '@mui/material'
 
+const CImg = styled('img')(({theme}) => ({
+  backgroundColor: 'blue',
+  display: 'flex', justifyContent: 'center', alignItems: 'center',
+  borderRadius: '20px',
+  boxShadow: '0.5em 1em 1em rgb(64, 64, 70)', 
+  width: '50%', 
+  [theme.breakpoints.down('sm')]: {
+    width: '100%', 
+    marginBottom: '30px',
+  },
+  [theme.breakpoints.down('md')]: {
+    height: '350px',
+  }
+}))
 
 const MovieInformation = () => {
   const {id} = useParams()
@@ -11,12 +25,14 @@ const MovieInformation = () => {
   console.log(data)
 
   const classes = {
-    containerSpaceAround: { display: 'flex', justifyContent: 'space-around', margin: '10px 0 !important', [theme.breakpoints.down('sm')]: {
+    containerSpaceAround: { 
+      display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', margin: '10px 10px !important', 
+      [theme.breakpoints.down('sm')]: {
       flexDirection: 'column',
-      flexWrap: 'wrap'
+      flexWrap: 'wrap',
     }},
     poster: {
-      backgroundColor: 'red'
+
     }
   }
 
@@ -39,7 +55,8 @@ const MovieInformation = () => {
   return (
     <Grid container style={classes.containerSpaceAround}>
       <Grid item sm={12} lg={4}>
-        <img style={classes.poster} src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`} alt={data?.title} />
+        <CImg style={classes.poster} src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`} alt={data?.title} />
+        {/* <img style={classes.poster} src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`} alt={data?.title} /> */}
       </Grid>
     </Grid>
   )
