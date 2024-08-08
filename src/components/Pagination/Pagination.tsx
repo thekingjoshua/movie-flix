023a1e172e9081743af1bd50635ca77eb619382f
KingJoshua <kingjayeriughe@gmail.com>
2024-08-08 16:46:52 +0100

    Added the Current page index

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,17 +1,19 @@
-import { Button, Typography } from '@mui/material'
 import React from 'react'
+import { Button, Typography } from '@mui/material'
 
 const Pagination = () => {
     const classes = {
         container: {},
         button: {},
-
+        pageNumber: {},
     }
+    const currentPage = 1
 
 
   return (
     <div style={classes.container}>
         <Button style={classes.button} variant='contained' color="primary" type="button">Prev</Button>
+        <Typography variant="h4" style={classes.pageNumber}>{currentPage}</Typography>
     </div>
   )
 }
