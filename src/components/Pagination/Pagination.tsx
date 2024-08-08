ec5e76e36e3d71345da2cb67c9d5c6edf36196eb
KingJoshua <kingjayeriughe@gmail.com>
2024-08-08 16:58:19 +0100

    Added styling

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Button, Typography } from '@mui/material'
+import { Button, Typography, useTheme } from '@mui/material'
 
 const Pagination = () => {
+    const theme = useTheme()
+    
     const classes = {
-        container: {},
-        button: {},
-        pageNumber: {},
+        container: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        button: {
+            margin: '30px 20px'
+        },
+        pageNumber: {
+            margin: '0 20px !important',
+            color: theme.palette.text.primary
+        },
     }
     const currentPage = 1
 
