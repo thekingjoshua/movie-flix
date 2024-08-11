92f8f54945ef20e943eda95e813aab9540fba78c
KingJoshua <kingjayeriughe@gmail.com>
2024-08-11 10:24:29 +0100

    Added code to map an display fav and watchlisted movies

--- src/components/RatedCards/RatedCards.tsx
+++ src/components/RatedCards/RatedCards.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material'
+import Movie from '../Movie/Movie'
 
-const RatedCards = () => {
+type RatedCardsProps = {
+    title: string,
+    data: {
+        results: []
+    }
+}
+
+const classes ={
+    container: {}
+}
+
+
+const RatedCards = ({title, data}: RatedCardsProps) => {
+    console.log(data?.results)
   return (
-    <div>RatedCards</div>
+    <Box>
+        <Typography variant='h5' gutterBottom>{title}</Typography>
+        <Box display="flex" flexWrap='wrap' style={classes.container}>
+            {data?.results.map((movie: {title: []; poster_path: string; id: number; vote_average: number;}, i) =>(
+                <Movie i={i} movie={movie} />
+            ))}
+        </Box>
+    </Box>
   )
 }
 
