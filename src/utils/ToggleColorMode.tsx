d39d6dc4ad7ff39cba6dee7c545857e46ba5bc6c
KingJoshua <kingjayeriughe@gmail.com>
2024-08-11 20:48:18 +0100

    Added ToggleColorMode

---
+++ src/utils/ToggleColorMode.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material'
+
+const toggleColorMode = () => {
+  return (
+    <div>toggleColorMode</div>
+  )
+}
+
+export default toggleColorMode
