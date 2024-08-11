b4dcbbb8ba315afa6d501f37e703950366d985c8
KingJoshua <kingjayeriughe@gmail.com>
2024-08-11 21:15:17 +0100

    Replaced ThemeProvider with ToggleContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
 import {Provider} from 'react-redux'
 
 import {store} from './app/store'
 
+import ToggleColorModeProvider from './utils/toggleColorMode'
+
 import './main.css'
-const theme = createTheme({})
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
     <BrowserRouter>
-    <ThemeProvider theme={theme}>
+    <ToggleColorModeProvider  >
       <App />
-    </ThemeProvider>
+    </ToggleColorModeProvider>
     </BrowserRouter>
     </Provider>
   </React.StrictMode>,
