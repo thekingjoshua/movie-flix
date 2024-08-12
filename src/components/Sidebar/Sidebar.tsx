aabbc54da12f204a5ee5fae980076cc1c7eae909
KingJoshua <kingjayeriughe@gmail.com>
2024-08-12 10:51:37 +0100

    Updated Genres Image to Invert in Dark Mode

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -42,7 +42,7 @@ const CATEGORIES = [
         imageLink: { display: 'flex', justifyContent: 'center', padding: '10% 0'}, 
         image: { width: '70%'}, 
         links: {color: theme.palette.text.primary, textDecoration: 'none'}, 
-        genreImage: {filter: theme.palette.mode === 'dark' ? 'invert(1)' : ''}
+        genreImage: {filter: theme.palette.mode === 'dark' && 'invert(1)',}
     }
     type GenreKeys = keyof typeof genreIcons;
 
